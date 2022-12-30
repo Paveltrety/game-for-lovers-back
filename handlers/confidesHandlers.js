52215f6fef54b8ae2a3cadb8e856b4d8fc7c97d2
Pavel Tretyakov <paveltretyakov@MacBook-Pro-Pavel-2.local>
2022-12-31 01:11:56 +0300

    add pagination

--- handlers/confidesHandlers.js
+++ handlers/confidesHandlers.js
@@ -2,8 +2,20 @@ import Confides from "../Schema/confides.js";
 
 export const getAllConfides = async (req, res) => {
   try {
-    const confidesCards = await Confides.find();
-    res.json(confidesCards);
+    const { page = 1, limit = 10 } = req.query;
+    const confidesCards = await Confides.find({})
+      .select({ _id: 0, __v: 0 })
+      .skip((page - 1) * limit)
+      .sort("id")
+      .limit(limit);
+
+    const count = await Confides.count();
+
+    res.json({
+      data: confidesCards,
+      totalPages: Math.ceil(count / limit),
+      currentPage: Number(page),
+    });
   } catch (error) {
     res.status(500).json(error);
   }
--- handlers/vulgarsHandlers.js
+++ handlers/vulgarsHandlers.js
@@ -2,8 +2,21 @@ import Vulgars from "../schema/vulgars.js";
 
 export const getAllVulgars = async (req, res) => {
   try {
-    const vulgarsCards = await Vulgars.find();
-    res.json(vulgarsCards);
+    const { page = 1, limit = 10 } = req.query;
+
+    const vulgarsCards = await Vulgars.find({})
+      .select({ _id: 0, __v: 0 })
+      .skip((page - 1) * limit)
+      .sort("id")
+      .limit(limit);
+
+    const count = await Confides.count();
+
+    res.json({
+      data: vulgarsCards,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    });
   } catch (error) {
     res.status(500).json(error);
   }
